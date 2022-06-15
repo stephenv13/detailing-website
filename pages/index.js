af624df8fbfbcfa4352937714368b8d9878b3be4
stephenv13 <stephenvogelmeier@gmail.com>
2022-06-14 20:35:32 -0400

    created nav bar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
+import logo from '/assets/car-logo.svg'
+import Image from 'next/dist/client/image';
 
-export default function Home() {
+export default function ButtonAppBar() {
   return (
-    <p>This is a test</p>,
 
-    <h1>This is another test</h1>
+    // HOME PAGE NAV BAR
+    <div className='nav-bar'>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Image src={logo} height={50} width={50}/>
+            <Typography className= "site-name" variant="h6" component="div" sx={{ flexGrow: 1 }}>Josh Griggs Detailing</Typography>
+            <Button className="contact" color="inherit">Contact Us</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+
   );
 }
