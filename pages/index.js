c23702bbde88e5b78d4b0b061c0a65b08b02308a
stephenv13 <stephenvogelmeier@gmail.com>
2022-06-14 22:38:18 -0400

    removed CSS styling

--- pages/index.js
+++ pages/index.js
@@ -8,17 +8,14 @@ export default function ButtonAppBar() {
   return (
 
     // HOME PAGE NAV BAR
-    <div className='nav-bar'>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
             <Image src={logo} height={50} width={50}/>
-            <Typography className= "site-name" variant="h6" component="div" sx={{ flexGrow: 1 }}>Josh Griggs Detailing</Typography>
-            <Button className="contact" color="inherit">Contact Us</Button>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>Josh Griggs Detailing</Typography>
+            <Button sx={{ fontWeight: 'bold' }} color="inherit">Contact Us</Button>
           </Toolbar>
         </AppBar>
       </Box>
-    </div>
-
   );
 }
